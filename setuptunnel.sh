e77e253707d0ae03e843cc5f8d6d332b2776f46c
Uri <urifallon.sc.9125268@gmail.com>
2025-11-06 13:20:15 +0700

    all

--- setuptunnel.sh
+++ setuptunnel.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-curl -X POST "https://api.cloudflare.com/client/v4/accounts/aed6351b715621d8592005d2b96b2022/tunnels" \
-     -H "Authorization: Bearer KDjypxaUbaHr93iSI3dWJzKF2YO2qxp4sYK52Ufy" \
-     -H "Content-Type: application/json" \
-     --data "{\"name\":\"openemr-tunnel\"}"
+CF_API_TOKEN="KDjypxaUbaHr93iSI3dWJzK52Ufy"
+CF_ACCOUNT_ID="aed6351b715621d8592005d2b96b202"
+TUNNEL_ID="6ae8a17d-9e51-4c2a-8850-22f30be2502f"
+DOMAIN="fatbeo.com"
+ZONE_NAME=${DOMAIN#*.}
+
+ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${ZONE_NAME}" \
+  -H "Authorization: Bearer ${CF_API_TOKEN}" \
+  -H "Content-Type: application/json" | jq -r '.result[0].id // empty')
+
+curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records" \
+  -H "Authorization: Bearer ${CF_API_TOKEN}" -H "Content-Type: application/json" \
+  --data "{\"type\":\"CNAME\",\"name\":\"${DOMAIN}\",\"content\":\"${TUNNEL_ID}.cfargotunnel.com\",\"ttl\":1,\"proxied\":true}" | jq
